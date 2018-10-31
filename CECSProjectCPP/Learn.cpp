cb3265fc029d1121384f64afaf9d5143dfa0e8b1
Vichaya Chantamoke <vichaya_chantamoke@hotmail.com>
2018-10-31 06:38:27 -0700

    practice binary shift

--- CECSProjectCPP/Learn.cpp
+++ CECSProjectCPP/Learn.cpp
@@ -11,7 +11,18 @@ using namespace std;
 
 int main()
 {
+	int a = 4;
+	char b[44];
 	cout << sizeof(int) << " " << sizeof(float) << endl;
+
+	_itoa_s(a, b, 2);
+	cout << b << endl;
+
+	a >>= 2;
+
+	_itoa_s(a, b, 2);
+	cout << b << endl;
+	cout << 1.0f / 3.0f << endl;
 
 	return 0;
 }
