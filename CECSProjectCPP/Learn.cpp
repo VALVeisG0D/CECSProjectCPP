d39855e25c8b0d479d2dd22e84e4d88a307c2171
Vichaya Chantamoke <vichaya_chantamoke@hotmail.com>
2018-11-03 06:55:53 -0700

    More playing with bitwise operation

--- CECSProjectCPP/Learn.cpp
+++ CECSProjectCPP/Learn.cpp
@@ -11,18 +11,24 @@ using namespace std;
 
 int main()
 {
-	int a = 4;
+	int a = 8;
 	char b[44];
 	cout << sizeof(int) << " " << sizeof(float) << endl;
 
 	_itoa_s(a, b, 2);
 	cout << b << endl;
 
-	a >>= 2;
-
-	_itoa_s(a, b, 2);
+	int c = 3;
+	int cc = c & 1;
+	c >>= 1;
+	int ccc = c & 1;
+	
+	_itoa_s(c, b, 2);
 	cout << b << endl;
-	cout << 1.0f / 3.0f << endl;
+	_itoa_s(cc, b, 2);
+	cout << b << endl;
+	_itoa_s(ccc, b, 2);
+	cout << b << endl;
 
 	return 0;
 }
