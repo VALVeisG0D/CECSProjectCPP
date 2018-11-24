bbb0ff26cd8a335e19be9e8d518982c0f69c39b0
Vichaya Chantamoke <vichaya_chantamoke@hotmail.com>
2018-11-24 06:27:45 -0800

    Add comments on needing to work for range of values, including negatives

--- CECSProjectCPP/Learn.cpp
+++ CECSProjectCPP/Learn.cpp
@@ -17,18 +17,18 @@ int main()
 
 	_itoa_s(a, b, 2);
 	cout << b << endl << endl;
-	
-	int inertia = 1;
-	int position = 0;
-	int change = 0;
-	int temp = 0;
+
+	int inertia = 1;	//Range of values: -3 - +3
+	int position = 0;	//Range of values: -MAXINT - +MAXINT
+	int change = 0;		//Range of values: -6 - +6
+	int temp = 0;		//Range of values: -1 - +1
 
 	for (int i = 0; i < 12; ++i)
 	{
 		change += inertia;
-		position += temp = (0xfff8 >> change) & 1;
+		position += temp = (0xfff8 >> change) & 1; //need to work for negatives too
 		cout << inertia << " " << change << " " << position << endl;
-		change -= temp * 3; // normalize the change when it crosses a threshold
+		change -= temp * 3; // normalize the change when it crosses a threshold. need to work for negatives too
 	}
 
 	_itoa_s(((0xfff8 >> 7) & 1), b, 2);
