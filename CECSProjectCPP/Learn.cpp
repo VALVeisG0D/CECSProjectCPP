44a61d5e0f331ecff2fb21267d7310e03e327999
VALVeisG0D <vichaya_chantamoke@hotmail.com>
2018-11-26 11:14:02 -0800

    Rethinking logic

--- CECSProjectCPP/Learn.cpp
+++ CECSProjectCPP/Learn.cpp
@@ -18,19 +18,20 @@ int main()
 	_itoa_s(a, b, 2);
 	cout << b << endl << endl;
 
-	int inertia = -2;	//Range of values: -3 - +3
+	int inertia = 2;	//Range of values: -3 - +3
 	int position = 0;	//Range of values: -MAXINT - +MAXINT
-	int change = 0;		//Range of values: -6 - +6
+	int change = 3;		//Range of values: -6 - +6
 	int temp = 0;		//Range of values: -1 - +1
 	int offset = 0;
 
 	for (int i = 0; i < 12; ++i)
 	{
-		change += inertia;
-		offset = change + 3;
-		position += temp = -(1 >> offset) + ((0xffc0 >> offset) & 1); //need to work for negatives too
+		change -= inertia;
+		position += (1 >> change);
+		//offset = change + 4;
+		//position += temp = -(1 >> offset) + ((0xff80 >> offset) & 1); //need to work for negatives too
 		cout << inertia << " " << change << " " << position << " " << temp << endl;
-		change -= temp * 3; // normalize the change when it crosses a threshold. need to work for negatives too
+		change += (1 >> change) * 3; // normalize the change when it crosses a threshold. need to work for negatives too
 	}
 
 	_itoa_s(-(0xffc0 >> 0), b, 2);
