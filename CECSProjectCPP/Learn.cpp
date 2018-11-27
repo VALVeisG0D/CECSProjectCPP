3da16a073d5da5871632863146679531640c165a
Vichaya Chantamoke <vichaya_chantamoke@hotmail.com>
2018-11-27 06:30:15 -0800

    Some more adjustment to logic accomodating negative inertia

--- CECSProjectCPP/Learn.cpp
+++ CECSProjectCPP/Learn.cpp
@@ -18,24 +18,23 @@ int main()
 	_itoa_s(a, b, 2);
 	cout << b << endl << endl;
 
-	int inertia = 2;	//Range of values: -3 - +3
+	int inertia = -2;	//Range of values: -3 - +3
 	int position = 0;	//Range of values: -MAXINT - +MAXINT
-	int change = 3;		//Range of values: -6 - +6
+	int change = 0;		//Range of values: -6 - +6
 	int temp = 0;		//Range of values: -1 - +1
 	int offset = 0;
 
-	for (int i = 0; i < 12; ++i)
+	for (int i = 0; i < 13; ++i)
 	{
-		change -= inertia;
-		position += (1 >> change);
-		//offset = change + 4;
-		//position += temp = -(1 >> offset) + ((0xff80 >> offset) & 1); //need to work for negatives too
-		cout << inertia << " " << change << " " << position << " " << temp << endl;
-		change += (1 >> change) * 3; // normalize the change when it crosses a threshold. need to work for negatives too
+		change += inertia;
+		offset = change + 6;
+		position += temp = -((8>> offset) & 1) + ((0xfe00 >> offset) & 1); //need to work for negatives too
+		cout << inertia << " " << change << " " << position << " " << offset << endl;
+		change += (((8 >> offset) & 1) + ((0xfe00 >> offset) & 1)) * -change; // normalize the change when it crosses a threshold. need to work for negatives too
 	}
 
-	_itoa_s(-(0xffc0 >> 0), b, 2);
-	cout << -(1 >> 0) << endl;
+	_itoa_s(8, b, 2);
+	cout << ((8 >> 3) & 1) << endl;
 
 	return 0;
 }
