75bea1e8e5690a6ba0e5a07efbef5352ae26214a
Vichaya Chantamoke <vichaya_chantamoke@hotmail.com>
2018-11-28 04:59:24 -0800

    Small code reoganization

--- CECSProjectCPP/Learn.cpp
+++ CECSProjectCPP/Learn.cpp
@@ -18,19 +18,22 @@ int main()
 	_itoa_s(a, b, 2);
 	cout << b << endl << endl;
 
-	int inertia = -3;	//Range of values: -3 - +3
-	int position = 0;	//Range of values: -MAXINT - +MAXINT
-	int change = 0;		//Range of values: -6 - +6
-	int temp = 0;		//Range of values: -1 - +1
+	int inertia = 2;			//Range of values: -3 - +3
+	int position = 0;			//Range of values: -MAXINT - +MAXINT
+	int change = 0;				//Range of values: -3 - +3
+	int positivePart = 0;		//Range of values: -1 - +1
+	int negativePart = 0;		//Range of values: -1 - +1
 	int offset = 0;
 
 	for (int i = 0; i < 13; ++i)
 	{
 		change += inertia;
 		offset = change + 6;
-		position += temp = -((0x000f >> offset) & 1) + ((0xfe00 >> offset) & 1); //need to work for negatives too
+		positivePart = ((0xfe00 >> offset) & 1);
+		negativePart = ((0x000f >> offset) & 1);
+		position += positivePart - negativePart; //need to work for negatives too
 		cout << inertia << " " << change << " " << position << " " << offset << endl;
-		change += (((0x000f >> offset) & 1) + ((0xfe00 >> offset) & 1)) * -change; // normalize the change when it crosses a threshold. need to work for negatives too
+		change += (negativePart + positivePart) * -change; // normalize the change when it crosses a threshold. need to work for negatives too
 	}
 
 	_itoa_s(8, b, 2);
